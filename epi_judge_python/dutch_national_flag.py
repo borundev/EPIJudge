1e604768ce62c57a3afbeb739cd485c4c570f69b
borundev <borundev@gmail.com>
2019-07-25 09:44:18 +0200

    solved some problems

--- epi_judge_python/dutch_national_flag.py
+++ epi_judge_python/dutch_national_flag.py
@@ -7,10 +7,61 @@ from test_framework.test_utils import enable_executor_hook
 RED, WHITE, BLUE = range(3)
 
 
-def dutch_flag_partition(pivot_index, A):
-    # TODO - you fill in here.
-    return
+def dutch_flag_partition_OnSq(pivot_index, A):
+    pivot=A[pivot_index]
 
+    for i in range(len(A)):
+        for j in range(i+1,len(A)):
+            if A[j]<pivot:
+                A[i],A[j]=A[j],A[i]
+                break
+
+    for i in reversed(range(len(A))):
+        if A[i]<pivot:
+            # second pass so if we reach here we are already past larger and equal elements.
+            break
+        for j in reversed(range(i)):
+            if A[j]>pivot:
+                A[i],A[j]=A[j],A[i]
+                break
+
+def dutch_flag_partition_On(pivot_index, A):
+    pivot = A[pivot_index]
+
+    smaller=0
+    for i in range(len(A)):
+        if A[i] < pivot:
+            A[i], A[smaller] = A[smaller], A[i]
+            smaller+=1
+
+    larger=len(A)-1
+
+    for i in reversed(range(len(A))):
+        if A[i] < pivot:
+            # second pass so if we reach here we are already past larger and equal elements.
+            break
+        if A[i] > pivot:
+            A[i], A[larger] = A[larger], A[i]
+            larger-=1
+
+def dutch_flag_partition_On_v2(pivot_index, A):
+    pivot = A[pivot_index]
+
+    smaller,equal,larger=0,0,len(A)
+    while equal<larger:
+
+        if A[equal] < pivot:
+            A[equal], A[smaller] = A[smaller], A[equal]
+            smaller+=1
+            equal+=1
+        elif A[equal] == pivot:
+            equal += 1
+        else:
+            larger-=1
+            A[equal], A[larger] = A[larger], A[equal]
+
+
+dutch_flag_partition=dutch_flag_partition_On_v2
 
 @enable_executor_hook
 def dutch_flag_partition_wrapper(executor, A, pivot_idx):
--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -2,8 +2,20 @@ from test_framework import generic_test
 
 
 def power(x, y):
-    # TODO - you fill in here.
-    return 0.0
+    result,power=1,y
+
+    # if power is negative we solve the problem for 1/x while flipping the sign of the power
+    if power<0:
+        power,x=-power,1.0/x
+    while power:
+
+        # the result is only updated if LSB of power is 1
+        if power & 1:
+           result*=x
+
+        # in all cases, whether of not result is updated power is shifted right and x->x^2
+        x,power=x**2,power>>1
+    return result
 
 
 if __name__ == '__main__':
