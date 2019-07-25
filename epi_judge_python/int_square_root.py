7f71456f50e0ebc690f88474df315c8d3b709f62
borundev <borundev@gmail.com>
2019-07-25 11:15:27 +0200

    solved int_square_root

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -2,8 +2,14 @@ from test_framework import generic_test
 
 
 def square_root(k):
-    # TODO - you fill in here.
-    return 0
+    left,right=0,k
+    while left<=right:
+        mid=(left+right)//2
+        if mid**2<=k: # this <= means that the value returned below is left-1
+            left=mid+1
+        else:
+            right=mid-1
+    return left-1
 
 
 if __name__ == '__main__':
