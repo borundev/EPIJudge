611916904a192c574215f53ecbdaf85909088d99
Borun Chowdhury <borun@lore.ai>
2019-07-26 15:52:41 +0200

    Bug fix

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -6,10 +6,10 @@ def plus_one(A):
     A[-1]+=1
     for i in reversed(range(1,len(A))):
         if A[i]==10:
-            A[i]==0
+            A[i]=0
             A[i-1]+=1
     if A[0]==10:
-        A[0]==0
+        A[0]=0
         A.insert(0,1)
     return A
 
