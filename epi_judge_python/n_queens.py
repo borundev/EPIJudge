2ceae7d01563c057d18da8f4a3a6423ebabbabb0
borundev <borundev@gmail.com>
2020-03-29 18:38:47 +0200

    A more concise version

--- epi_judge_python/n_queens.py
+++ epi_judge_python/n_queens.py
@@ -4,38 +4,23 @@ from test_framework import generic_test
 
 
 def n_queens(n: int) -> List[List[int]]:
-    """
-    Returns all possible placements of non attacking queens on nxn chessboard. The information is
-    returned as a list of list where the inner list containes the columns of the queens
 
-    :param n: number of queens
-    :return:
-    """
+    def helper(row):
 
-    def helper_func(row):
-        """
-        Tries to places a queen of row. If this is more than the number of queens it means that all
-        the queens have been successfully placed so the placements are appended to the master result.
-        Otherwise if the placement is valid, the function is called for the next row.
-        :param row:
-        :return:
-        """
         if row==n:
-            result.append(col_result.copy())
-        else:
-            for col in range(n):
-                for r,c in enumerate(col_result):
-                    if abs(c-col) in (0,row-r):
-                        break
-                else:
-                    col_result.append(col)
-                    helper_func(row+1)
-                    col_result.pop()
+            results.append(partial_results.copy())
+            return
 
-    result=[]
-    col_result=[]
-    helper_func(0)
-    return result
+        for col in range(n):
+            if not any([abs(c-col) in (0,row-r) for r,c in enumerate(partial_results)]):
+                partial_results.append(col)
+                helper(row+1)
+                partial_results.pop()
+
+    results =[]
+    partial_results=[]
+    helper(0)
+    return results
 
 
 def comp(a, b):
--- problem_mapping.js
+++ problem_mapping.js
@@ -65,7 +65,7 @@ problem_mapping = {
                 "passed": 0,
                 "total": 10000
             },
-            "Python: closest_int_same_weight.py": { 
+            "Python: closest_int_same_weight.py": {
                 "passed": 0,
                 "total": 10000
             }
@@ -1974,7 +1974,7 @@ problem_mapping = {
                 "total": 10
             },
             "Python: hanoi.py": {
-                "passed": 0,
+                "passed": 10,
                 "total": 10
             }
         },
@@ -2002,7 +2002,7 @@ problem_mapping = {
                 "total": 10
             },
             "Python: n_queens.py": {
-                "passed": 0,
+                "passed": 10,
                 "total": 10
             }
         },
@@ -2044,7 +2044,7 @@ problem_mapping = {
                 "total": 120
             },
             "Python: combinations.py": {
-                "passed": 0,
+                "passed": 120,
                 "total": 120
             }
         },
