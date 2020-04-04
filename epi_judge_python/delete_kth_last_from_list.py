46f68d59697f58173d690cf5c073c3e0c2964b19
borundev <borundev@gmail.com>
2020-04-04 14:50:30 +0200

    Solved

--- epi_judge_python/delete_kth_last_from_list.py
+++ epi_judge_python/delete_kth_last_from_list.py
@@ -6,28 +6,21 @@ from test_framework import generic_test
 
 # Assumes L has at least k nodes, deletes the k-th last node in L.
 def remove_kth_last(L: ListNode, k: int) -> Optional[ListNode]:
+
     dummy_head=ListNode(0,L)
+    previous_to_delete=dummy_head
 
-    p_fast=L
-
-    # p_slow will point at the node prior to the one to delete
-    p_slow=dummy_head
-
-    # we advance the fast pointer k steps
+    # make lead point to previous_to_delete + (k+1)
+    lead=dummy_head.next
     for _ in range(k):
-        p_fast=p_fast.next
+        lead=lead.next
 
-    # if it is already at None we remove the first element (eg L=[2,1] and k=2)
-    # otherwise we advance both till p_fast does not reach the tail
+    # move lead to point to the end
+    while lead:
+        previous_to_delete,lead=previous_to_delete.next,lead.next
 
-    while p_fast:
-        p_slow=p_slow.next
-        p_fast=p_fast.next
-
-    p_slow.next=p_slow.next.next
-
+    previous_to_delete.next=previous_to_delete.next.next
     return dummy_head.next
-
 
 
 if __name__ == '__main__':
