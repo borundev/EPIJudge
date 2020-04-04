4dc887f16f924a63de3239c65b0bb937bb01780b
borundev <borundev@gmail.com>
2020-04-04 13:37:53 +0200

    Cleaner code

--- epi_judge_python/sorted_lists_merge.py
+++ epi_judge_python/sorted_lists_merge.py
@@ -5,19 +5,18 @@ from test_framework import generic_test
 
 def merge_two_sorted_lists(L1: Optional[ListNode],
                            L2: Optional[ListNode]) -> Optional[ListNode]:
-    dummy_head=ListNode()
-    p=dummy_head
-    while (L1 and L2):
-        if L1.data<L2.data:
-            p.next=L1
-            L1=L1.next
-            p=p.next
-        else:
-            p.next=L2
-            L2=L2.next
-            p=p.next
+    dummy_head=current=ListNode()
 
-    p.next=L1 or L2
+    while L1 and L2:
+        if L1.data<L2.data:
+            current.next=L1
+            L1=L1.next
+        else:
+            current.next = L2
+            L2 = L2.next
+        current = current.next
+
+    current.next=L1 or L2
 
     return dummy_head.next
 
