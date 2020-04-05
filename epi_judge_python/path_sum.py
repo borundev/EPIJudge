cff417fc903f7b352c7f04494d5ad0f661c853ee
borundev <borundev@gmail.com>
2020-04-05 18:46:23 +0200

    solved

--- epi_judge_python/path_sum.py
+++ epi_judge_python/path_sum.py
@@ -3,9 +3,26 @@ from test_framework import generic_test
 
 
 def has_path_sum(tree: BinaryTreeNode, remaining_weight: int) -> bool:
-    # TODO - you fill in here.
-    return True
 
+    def helper(tree,remaining_weight):
+        if not tree:
+            return False
+        remaining_weight -= tree.data
+
+        # The ONLY condition we can get match is if the left node ends up with remaning_weight 0
+        if not tree.left and not tree.right:
+            if remaining_weight==0:
+                # Bypass the stack
+                raise Exception()
+            else:
+                return False
+        return helper(tree.left,remaining_weight) or helper(tree.right,remaining_weight)
+
+    try:
+        helper(tree,remaining_weight)
+    except:
+        return True
+    return False
 
 if __name__ == '__main__':
     exit(
