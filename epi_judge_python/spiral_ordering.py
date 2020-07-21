d87799e45b706d640a51ab880a8a251b0268e167
Borun Chowdhury <borundev@gmail.com>
2020-07-21 11:01:44 +0200

    solved

--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -4,8 +4,39 @@ from test_framework import generic_test
 
 
 def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+
+    n=len(square_matrix)
+
+    def helper(offset):
+        """
+        Fill the outer ring at offset
+
+        :param offset:
+        :return:
+        """
+
+        # if the square is odd sized and we have reached the center then there is only one element
+        if offset == n-offset-1:
+            return [square_matrix[offset][offset]]
+
+        partial_result = []
+
+        partial_result.extend(square_matrix[offset][offset:~offset])
+
+        for r in range(offset,n-offset-1):
+            partial_result.append(square_matrix[r][~offset])
+
+        partial_result.extend(square_matrix[~offset][~offset:offset:-1])
+
+        for r in range(n-offset-1,offset,-1):
+            partial_result.append(square_matrix[r][offset])
+
+        return partial_result
+
+    result = []
+    for offset in range((n-1)//2+1):
+        result += helper(offset)
+    return result
 
 
 if __name__ == '__main__':
