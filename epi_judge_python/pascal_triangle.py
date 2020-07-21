22f9b823c02ef28b95477c75f214e39783eeb8f4
Borun Chowdhury <borundev@gmail.com>
2020-07-21 11:41:04 +0200

    solved

--- epi_judge_python/pascal_triangle.py
+++ epi_judge_python/pascal_triangle.py
@@ -4,8 +4,16 @@ from test_framework import generic_test
 
 
 def generate_pascal_triangle(n: int) -> List[List[int]]:
-    # TODO - you fill in here.
-    return []
+    if n==0:
+        return []
+    all_data=[[1]]
+    for i in range(n-1):
+        new_data=[1]
+        for k in range(len(all_data[-1])-1):
+            new_data.append(all_data[-1][k]+all_data[-1][k+1])
+        new_data.append(1)
+        all_data.append(new_data)
+    return all_data
 
 
 if __name__ == '__main__':
