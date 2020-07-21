816d1d4352a04900877a272c306d738e3d1a3ede
Borun Chowdhury <borundev@gmail.com>
2020-07-21 11:30:43 +0200

    solved

--- epi_judge_python/matrix_rotation.py
+++ epi_judge_python/matrix_rotation.py
@@ -4,8 +4,29 @@ from test_framework import generic_test
 
 
 def rotate_matrix(square_matrix: List[List[int]]) -> None:
-    # TODO - you fill in here.
-    return
+    n = len(square_matrix)
+
+    def rotate_layer(offset):
+        """
+        This is a very straightforward function and the only care needs to be in nothing which
+        index goes to which one
+        :param offset:
+        :return:
+        """
+        for idx in range(offset,n-1-offset):
+            (square_matrix[offset][idx],
+            square_matrix[~idx][offset],
+            square_matrix[~offset][~idx],
+            square_matrix[idx][~offset]) = \
+                (square_matrix[~idx][offset],
+                square_matrix[~offset][~idx],
+                square_matrix[idx][~offset],
+                square_matrix[offset][idx])
+
+
+    for offset in range(n//2):
+        rotate_layer(offset)
+
 
 
 def rotate_matrix_wrapper(square_matrix):
